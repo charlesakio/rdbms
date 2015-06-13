40122232fe2c874db1638b2f042e2b383708f1cd
Charles Javelona <charlesjavelona@gmail.com>
2015-06-12 23:05:52 -0400

    Create insert_options.sql

---
+++ insert_options.sql
@@ -0,0 +1,21 @@
+                       
+INSERT INTO options (ocode, odesc                   ,  ocost,  olist)
+             VALUES ('CD2', 'CD PLAYER'             , 110.00, 195.00);
+INSERT INTO options (ocode, odesc                   ,  ocost,  olist)
+             VALUES ('ED3', 'ELECTRONIC INSTRUMENTS', 423.00, 557.00);
+INSERT INTO options (ocode, odesc                   ,  ocost,  olist)
+	     VALUES ('M24', 'TAPE DECK'		    , 210.00, 285.00);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('R41', 'RECLINING BUCKET SEATS', 305.00, 420.00);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('S22', 'TINTED SUN ROOF'	    , 333.00, 386.00);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('S20', 'SKI RACK'		    , 212.00, 285.00);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('U20', 'BLACK UPHOLSTRY'	    ,  88.00, 106.48);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('M14', 'STEREO RADIO'	    , 292.00, 350.00);
+INSERT INTO options (ocode, odesc		    ,  ocost,  olist)
+	     VALUES ('E01', 'EXTENDED WARRANTY'	    , 270.50, 345.00);
+Jove (Text)   [options.sql:3]  "options.sql"  -  ~/sql                  (23:05)
+: popd ~/sql 
